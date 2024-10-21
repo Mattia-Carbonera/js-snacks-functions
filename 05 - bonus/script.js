bf8792329d649dc5bbdf5fb81e8c6906e8cd762e
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-21 18:17:25 +0200

    05 - bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,32 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
 
+const name = "Mario";
+
+// Dichiara la funzione qui.
+
+let data = new Date();
+
+let hours = data.getHours();
+
+console.log(hours);
+
+if (hours <= 13) {
+  alert("Buongiorno " + name);
+}
+
+if (hours >= 17) {
+  alert("Buonasera " + name);
+}
+
+if (hours > 13 && hours < 17) {
+  alert("Buon pomeriggio " + name);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
